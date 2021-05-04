4c6791a561266d8bc9f8b036aaf726e3a0eed6d3
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-05-04 16:34:31 +0500

    Updated Apps.js

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,13 +2,13 @@ import Constants from "expo-constants";
 
 const settings = {
   dev: {
-    apiUrl: "http://192.168.1.9:9000/api",
+    apiUrl: "https://boiling-waters-86040.herokuapp.com/api",
   },
   staging: {
-    apiUrl: "http://192.168.1.9:9000/api",
+    apiUrl: "https://boiling-waters-86040.herokuapp.com/api",
   },
   prod: {
-    apiUrl: "http://192.168.1.9:9000/api",
+    apiUrl: "https://boiling-waters-86040.herokuapp.com/api",
   },
 };
 
