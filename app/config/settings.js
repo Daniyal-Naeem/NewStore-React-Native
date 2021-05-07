e338c62a72df68d2dd3b00fde88dca89a0f73eb3
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-05-07 05:46:38 +0500

    test

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,13 +2,13 @@ import Constants from "expo-constants";
 
 const settings = {
   dev: {
-    apiUrl: "https://boiling-waters-86040.herokuapp.com/api",
+    apiUrl: "https://react-native-crud-app.herokuapp.com/api/",
   },
   staging: {
-    apiUrl: "https://boiling-waters-86040.herokuapp.com/api",
+    apiUrl: "https://react-native-crud-app.herokuapp.com/api/",
   },
   prod: {
-    apiUrl: "https://boiling-waters-86040.herokuapp.com/api",
+    apiUrl: "https://react-native-crud-app.herokuapp.com/api/",
   },
 };
 
